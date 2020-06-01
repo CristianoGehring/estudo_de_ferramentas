650783ae620e764dd247f483ff8876bde0e76af9
Cristiano Gehring <cristiano.gehring@gmail.com>
2020-06-01 15:15:25 -0300

    adicionando dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
+require('dotenv').config()
 var express = require('express')
 var app = express()
-
 const ObjectId = require('mongodb').ObjectID
 const MongoClient = require('mongodb').MongoClient
-const uri = "mongodb://patrickisidoro:nuzor1539@ds133279.mlab.com:33279/crud-nodejs"
+const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@cluster0-lozjt.mongodb.net/test?retryWrites=true&w=majority"
+
 
 MongoClient.connect(uri, (err, client) => {
   if (err) return console.log(err)
-  db = client.db('crud-nodejs') // coloque o nome do seu DB
+  db = client.db('estudo') // coloque o nome do seu DB
 
   app.listen(3000, () => {
     console.log('Server running on port 3000')
@@ -15,7 +16,9 @@ MongoClient.connect(uri, (err, client) => {
 })
 
 app.get('/', function (req, res) {
-  // const cursor = db.collection('data').find()
-  // console.log(cursor)
-  res.send('Hello World!')
+  db.collection('data').find().toArray((err, results) => {
+    if (err) return console.log(err)
+    console.log(results)
+    res.send('Hello World!')
+  })
 })
