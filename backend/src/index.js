738b617d88eeb467a466f991ae88187f395568d0
Cristiano Gehring <cristiano.gehring@gmail.com>
2020-06-01 14:05:33 -0300

    Instalando o MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+
+const ObjectId = require('mongodb').ObjectID
+const MongoClient = require('mongodb').MongoClient
+const uri = "mongodb://patrickisidoro:nuzor1539@ds133279.mlab.com:33279/crud-nodejs"
+
+MongoClient.connect(uri, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('crud-nodejs') // coloque o nome do seu DB
+
+  app.listen(3000, () => {
+    console.log('Server running on port 3000')
+  })
+})
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  // const cursor = db.collection('data').find()
+  // console.log(cursor)
+  res.send('Hello World!')
+})
