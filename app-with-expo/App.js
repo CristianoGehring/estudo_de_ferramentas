b96501b57c72f66d01e1133810cbf0ae94dfbd2f
GGehrinG <guilherme.gehring@gmail.com>
2020-06-06 11:56:05 -0300

    Merge

--- app-with-expo/App.js
+++ app-with-expo/App.js
@@ -2,13 +2,5 @@ import React from 'react';
 import Routes from './src/routes';
 
 export default function App() {
-<<<<<<< HEAD
   return <Routes />
-=======
-  return (
-    <View style={styles.container}>
-      <Text>FEATURE</Text>
-    </View>
-  );
->>>>>>> acesso-camera
 }
