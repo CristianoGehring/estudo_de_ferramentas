a3c8346764859a14d2ad6db5ad337d17b10d5eee
Cristiano Gehring <cristiano.gehring@gmail.com>
2020-06-09 17:48:53 -0300

    ajuste

--- app-with-expo/App.js
+++ app-with-expo/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { StatusBar } from 'react-native'
 import Routes from './src/routes';
 
-export default function App() {
+export default function App(props) {
+  console.log(props)
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#000" />
