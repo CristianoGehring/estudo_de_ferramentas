8e6599e0dc0cdcedf288f515b61ff044317df4ee
Cristiano Gehring <cristiano.gehring@gmail.com>
2020-06-08 21:55:32 -0300

    iniciando calendário

--- app-with-expo/src/routes.js
+++ app-with-expo/src/routes.js
@@ -3,11 +3,13 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import Inicial from './pages/Inicial';
 import HelloWorld from './pages/HelloWorld';
 import UploadImagem from './pages/UploadImagem';
+import Calendar from './pages/Calendar';
 
 const Routes = createAppContainer(
   createSwitchNavigator({
     Inicial,
     UploadImagem,
+    Calendar,
     HelloWorld
   })
 );
