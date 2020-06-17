aa3b4ca2a20edff363353a41d70c5ced70bc981c
Hpasa <henriquepasa@hotmail.com>
2020-06-16 22:14:16 -0300

    testes com um pacote para ter os componentes do material design

--- app-with-expo/App.js
+++ app-with-expo/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import Routes from './src/routes';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#394051',
+    accent: '#ff5b00',
+  },
+};
 
 export default function App() {
-  return <Routes />
+  return (
+    <PaperProvider theme={theme}>
+      <Routes />
+    </PaperProvider>
+  );
 }
--- app-with-expo/src/pages/Inicial.js
+++ app-with-expo/src/pages/Inicial.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, useTheme } from 'react-native-paper';
 
 export default function Example({ navigation }) {
   async function navegar() {
     navigation.navigate('HelloWorld');
   }    
 
+  const { colors } = useTheme();
+  var teste = false;
+
   return (
     <View style={styles.container}>
-      <Button title="Hello World" onPress={ () => navigation.navigate('HelloWorld') } />
-      <Button title="Upload Imagem" onPress={ () => navigation.navigate('UploadImagem') } />
+      <Button style={styles.button} icon="camera" mode="contained" onPress={ () => navigation.navigate('UploadImagem') } >Upload Imagem</Button>
+      <Button style={styles.button} mode="outlined" color={teste ? colors.primary : colors.accent} compact={false} onPress={ () => navigation.navigate('HelloWorld') } >Hello World</Button>
     </View>
   );
 }
@@ -21,4 +25,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    width: '90%',
+    marginVertical: 6,
+  }
 });
