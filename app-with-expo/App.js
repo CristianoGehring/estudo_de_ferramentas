d52504a4fb1b6a0625041aaa41d1eb2bb84bc481
Hpasa <henriquepasa@hotmail.com>
2020-06-17 21:33:38 -0300

    Adicionado uma personalização

--- app-with-expo/App.js
+++ app-with-expo/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { StatusBar } from 'react-native'
 import Routes from './src/routes';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#2a9d8f',
+    accent: '#e76f51',
+    background: '#264653'
+  },
+};
 
 export default function App(props) {
   console.log(props)
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#000" />
-      <Routes />
+      <PaperProvider theme={theme}>
+        <StatusBar barStyle="light-content" backgroundColor="#000" />
+        <Routes />
+      </PaperProvider>
     </>
   )
 }
--- app-with-expo/src/pages/Inicial.js
+++ app-with-expo/src/pages/Inicial.js
@@ -1,46 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Button, useTheme } from 'react-native-paper';
 
 export default function Example({ navigation }) {
+  const { tema } = useTheme();
+
   const buttons = [
     {
-      title: 'Hello Word',
-      route: 'HelloWorld'
-    },
-    {
       title: 'Upload Imagem',
-      route: 'UploadImagem'
+      route: 'UploadImagem',
+      icone: 'camera'
     },
     {
       title: 'Calendário',
-      route: 'Calendar'
-    }
+      route: 'Calendar',
+    },
+    {
+      title: 'Hello Word',
+      route: 'HelloWorld',
+      style: 'text'
+    },
   ]
 
   var renderButton = (button, i) => {
     return (
       <View style={styles.buttons} key={i}>
-        <Button title={button.title} onPress={ () => navigation.navigate(`${button.route}`) } />
+        <Button icon={button.icone}  mode={button.style ? button.style : 'contained'} onPress={ () => navigation.navigate(`${button.route}`) } >{button.title}</Button>
       </View>
     )
   };
 
   return (
-    <View style={styles.container}>
-      { buttons.map(renderButton) }
+    <View style={styles.box}>
+      <Image
+        style={styles.tinyLogo}
+        source={require('../../assets/logo.png')}
+      />
+        <View style={styles.container}>
+          { buttons.map(renderButton) }
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  box: {
+    flex: 1,
+    backgroundColor: '#264653',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
   buttons: {
     width: '90%',
     marginVertical: 6
-  }
+  },
+  tinyLogo: {
+    marginTop: 24,
+    width: 120,
+    height: 32,
+  },
 });
