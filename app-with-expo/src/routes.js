d1db21a5954cb0f7133f7d663fe2aa1890188266
Hpasa <henriquepasa@hotmail.com>
2020-06-18 22:28:09 -0300

    Para instalar a dependência de navegação estacada tinha que ter yarn

--- app-with-expo/src/routes.js
+++ app-with-expo/src/routes.js
@@ -1,4 +1,9 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React from 'react'
+import { Image } from 'react-native'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import logo from './assets/logo.png';
 
 import Inicial from './pages/Inicial';
 import HelloWorld from './pages/HelloWorld';
@@ -6,11 +11,19 @@ import UploadImagem from './pages/UploadImagem';
 import Calendar from './pages/Calendar';
 
 const Routes = createAppContainer(
-  createSwitchNavigator({
+  createStackNavigator({
     Inicial,
     UploadImagem,
     Calendar,
     HelloWorld
+  }, {
+    defaultNavigationOptions: {
+      headerTitleAlign: 'center',
+      headerTitle: () => <Image source={logo} />,
+      headerStyle: {
+        backgroundColor: '#264653'
+      }
+    }
   })
 );
 
